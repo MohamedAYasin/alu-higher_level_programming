bd3cd22bf534c680e23288a966e5081ea3243b2d
MohamedAYasin <m.yasin@alustudent.com>
2023-02-07 09:33:47 -0800

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+str = "Python is an interpreted, interactive, object-oriented programming\
+
+                language that combines remarkable power with very clear syntax"
+
+                str = str[39:67] + str[107:112] + str[:6]
+
+                print(str)
+
+
