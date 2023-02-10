d53dd3dc2720f8ec6d7927c5ad8d5c6be2c05cb0
MohamedAYasin <m.yasin@alustudent.com>
2023-02-10 05:48:05 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
+
     
-if chr(letter) is not 'q' and chr(letter) is not 'e':
+if chr(letter) is not '101' and chr(letter) is not '113':
     print("{}".format(chr(letter)), end="")
