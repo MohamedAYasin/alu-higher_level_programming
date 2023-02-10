50ae0d1ef10163e1f95464901d4d829533482787
MohamedAYasin <m.yasin@alustudent.com>
2023-02-10 11:13:27 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    if (char =! 101 and char =! 113):
-        print("{:c}".format(char), end="")
+
+for i in range(97, 122 + 1):
+    if ((i == 113) or (i == 101)):
+        continue
+    print("{:c}".format(i), end='')
