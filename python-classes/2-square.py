fcb0f6aa8ad46b528d31580f21e0644a28d5f40c
MohamedAYasin <m.yasin@alustudent.com>
2023-03-17 11:47:22 -0700

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A class that defines a square"""
+
+
+class Square:
+    """Creates a square.
+    Private instance attributes: size
+    """
+
+    def __init__(self, size=0):
+        """Initializes data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = 
