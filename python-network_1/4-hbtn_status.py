b8e4a379e3ea35ca0e6eedaa36f4b04671edeef3
MohamedAYasin <m.yasin@alustudent.com>
2023-04-06 23:37:26 +0000

    Python Network 1

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+import requests
+
+if __name__ == '__main__':
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
