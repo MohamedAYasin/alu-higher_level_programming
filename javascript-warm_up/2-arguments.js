42c822b21f16f8184b9a741ea812b1f2e14695bf
MohamedAYasin <m.yasin@alustudent.com>
2023-06-06 10:57:39 -0700

    JS basics

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/nin/node
+
+const {argv} = process;
+
+const argvLen = argv.length;
+
+if (argvLen < 3) console.log('No argument');
+if (argvLen === 3) console.log('Argument found');
+if (argvLen > 3) console.log('Arguments found');
