c933ff215faad438e91e801d5b8bbe7fa8cfa36e
MohamedAYasin <m.yasin@alustudent.com>
2023-06-06 09:43:09 -0700

    JS basics

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const languages = ("C is fun \n Python is cool \n JavaScript is amazing \n");
-    console.log(languages);
+const languages = ('C is fun \n Python is cool \n JavaScript is amazing \n');
+console.log(languages);
