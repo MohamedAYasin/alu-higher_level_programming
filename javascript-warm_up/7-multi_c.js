f851742d521253eb5d3bc76deed111896d3e56cd
MohamedAYasin <m.yasin@alustudent.com>
2023-06-09 09:46:13 -0700

    JS Basics

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+#!/usr/bin/node
+
+const { argv } = process;
+
+const numArg = Number(argv[2]);
+
+if (!numArg) console.log('Missing number of occurrences');
+else {
+  for (let i = 0; i < numArg; i++) {
+    console.log('C is fun');
+  }
+}
